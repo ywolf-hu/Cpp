63ed46fec2e8837098deb6b1a96f5c504f3ed668
ywolf-hu <hlf0312@163.com>
2015-02-23 14:38:16 +0800

    c01-07 answer

---
+++ TICPP2ndV2/code/C01/exercises/07.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+class anyClass
+{
+public:
+  anyClass(){
+    cout << "anyClass Constructor" << endl;
+  }
+
+  ~anyClass(){
+    cout << "anyClass Destructor" << endl;
+  }
+class exceptionClass
+{
+public:
+  exceptionClass(){
+    cout << "exceptionClass Constructor" << endl;
+  }
+  ~exceptionClass(){
+    cout << "exceptionClass Destructor" << endl;
+  }
+};
+  void anyFunc(){
+    exceptionClass* pexceptionObj = new exceptionClass;
+    throw *pexceptionObj;
+  }
+};
+
+int main()
+{
+    try{
+      anyClass anyclass;
+      anyclass.anyFunc();
+    }
+    catch (anyClass::exceptionClass& x){
+      cout << "One exception thrown" << endl;
+    }
+  return 0;
+}
